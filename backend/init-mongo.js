a766e2d9c5a352900f86a5c31d8cbe62fabef1fc
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-23 15:21:33 +0100

    MongoDB init

---
+++ backend/init-mongo.js
@@ -0,0 +1,15 @@
+// Create the admin user
+db = db.getSiblingDB('admin');
+db.createUser({
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  roles: [{ role: 'root', db: 'admin' }]
+});
+
+// Create the application user for a specific database
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
+db.createUser({
+  user: process.env.MONGO_INITDB_USER,
+  pwd: process.env.MONGO_INITDB_PASSWORD,
+  roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }]
+});
