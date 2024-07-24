b735f6cc90aac3df8455b18a82835e8df861303f
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-24 16:26:38 +0100

    console log

--- backend/seed.js
+++ backend/seed.js
@@ -5,32 +5,32 @@ import env from './src/config/environment.js'; // Adjust path as needed
 
 const seedDatabase = async () => {
   try {
-    // Connect to MongoDB
     const dbURI = `mongodb://${env.DB_HOST}/${env.DB_NAME}`;
     await mongoose.connect(dbURI, {
       user: env.DB_USER,
       pass: env.DB_PASS,
     });
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB to populate DB --------------------x-x--x-x-x-x-x-');
 
-    // Check if data already exists
     const usersExist = await User.countDocuments() > 0;
     if (usersExist) {
       console.log('Database already seeded');
+      mongoose.disconnect();
       return;
     }
 
-    // Seed data
     const user = await User.create({
       username: 'testuser',
-      password: 'testpassword', // You might want to hash passwords in real apps
+      password: 'testpassword',
     });
-    
+    console.log('User created:', user);
+
     await Roadtrip.create({
       userId: user._id,
       name: 'Test Roadtrip',
       locations: ['Location1', 'Location2'],
     });
+    console.log('Roadtrip created');
 
     console.log('Database seeded successfully');
     mongoose.disconnect();
