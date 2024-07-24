42dc4e0926cd49387bb3c47aa5dfa8b8bbb0c774
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-24 14:55:48 +0100

    changed variables minor

--- backend/src/config/environment.js
+++ backend/src/config/environment.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  DB_HOST: process.env.DB_HOST,
+  DB_HOST: process.env.DB_HOST || 'mongo:27017',
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
