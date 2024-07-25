ee05a695180124341adcee5efa05b1354efdd296
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-25 14:39:30 +0100

    added /me to get user details

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -76,3 +76,17 @@ export const login = async (req, res) => {
     res.status(500).send('Server error');
   }
 };
+
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { register, login } from '../controllers/userController.js';
+import { register, login, getUserProfile } from '../controllers/userController.js';
+import authenticateToken from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/me', authenticateToken, getUserProfile);
 
 export default router;
