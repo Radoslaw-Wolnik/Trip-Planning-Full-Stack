80cd6109d90c670e833fc0e5e082b0c91e0c4519
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-26 13:21:18 +0100

    moved function from TRipsController to utils

---
+++ backend/src/utils/generateInvitationCode.js
@@ -0,0 +1,9 @@
+// Helper function to generate a random string
+export const generateInvitationCode = (length = 6) => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+};
