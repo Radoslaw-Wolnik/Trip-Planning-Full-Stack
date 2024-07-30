9f72f1a1848b375430d62a72e7b74d72cd1eccc9
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-30 11:06:44 +0100

    global types !

---
+++ frontend/src/types.ts
@@ -0,0 +1,53 @@
+export interface User {
+    _id: string;
+    username: string;
+    profilePicture?: string;
+  }
+  
+  export interface FullUser extends User {
+    email: string;
+    isVerified?: boolean;
+  }
+  
+  export interface Place {
+    name: string;
+    date: Date;
+    latitude: number;
+    longitude: number;
+    order: number;
+  }
+  
+  export interface Trip {
+    _id: string;
+    title: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+    places?: Place[];
+    creator: User;
+    sharedWith: User[];
+    invitationCode?: string;
+  }
+  
+  // You can also include the request payload interfaces here if you want them to be globally available
+  export interface Credentials {
+    email: string;
+    password: string;
+  }
+  
+  export interface UserData {
+    username: string;
+    email: string;
+    password: string;
+  }
+  
+  export interface TripData {
+    title: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+    places?: Place[];
+    creator?: string;
+    sharedWith?: string[];
+    invitationCode?: string;
+  }
