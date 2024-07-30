fb1c1534ad0beca24a2e3f232d3895cfa1468775
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-07-30 11:07:10 +0100

    helper function to show correctly profile pictures

---
+++ frontend/src/utils/imageUtils.ts
@@ -0,0 +1,7 @@
+//import { API_URL } from '../config';
+
+const API_URL = 'http://localhost:5000';
+
+export const getFullImageUrl = (imagePath: string) => {
+  return imagePath ? `${API_URL}${imagePath}` : '/default-avatar.png';
+};
