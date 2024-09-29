6420e3ee3682b4665fc2944ead5bbbef62f876e6
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-09-29 15:17:29 +0200

    renamed

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -6,18 +6,13 @@ import upload from '../middleware/upload.middleware.js';
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', authenticateToken, logout);
+
 
 // it first executes authenticateToken then getUserProfile so in auth we decode the token to id of user
 router.get('/me', authenticateToken, getUserProfile);
 
 router.put('/upload-profile-picture', authenticateToken, upload.single('profilePicture'), uploadProfilePicture);
-router.put('/change-password', authenticateToken, changePassword);
 
-router.post('/send-verification', authenticateToken, sendVerificationEmail);
-router.get('/verify-email/:token', verifyEmail);
 
 router.get('/:userId', authenticateToken, getOtherUserProfile);
 router.get('/:userId/trips', authenticateToken, getUserTrips);
