8d6d7de68fa5382c65958a08aa936cfc16292d49
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-10-03 16:04:45 +0200

    now in ts

--- backend/src/utils/extract-token.util.js
+++
@@ -1,7 +0,0 @@
-// utils/tokenExtractor.js
-const extractToken = (req) => {
-    const authHeader = req.headers['authorization'];
-    return authHeader && authHeader.split(' ')[1];
-};
-
-export default extractToken;
